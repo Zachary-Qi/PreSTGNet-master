d0a9e16be973dba6e0804b72b45472968722cc77
Zachary <155824252+Zachary-Qi@users.noreply.github.com>
2026-03-09 10:06:37 +0800

    Update and rename main_best.py to test.py

--- test.py
+++ test.py
@@ -7,181 +7,42 @@ import torch.distributed as dist
 
 from tqdm import tqdm
 from datetime import datetime
-from lib.engine1 import trainer
+from lib.engine import trainer
 from torch.utils.data import DataLoader, DistributedSampler
 from lib.utils import *
 
 
-# 加载配置文件
-DATASET_NAME = "PeMS04"
-MODEL_NAME = "lsfgcn"
+DATASET_NAME = "PeMS08"
+MODEL_NAME = "PreSTGNet"
 MODE = "Train" # only Train or PreTrain
-# 加载训练好的掩码模型
-PRE_TRAINED_WEIGHT_PATH = "./experiments/PeMS04_lsfgcn/PreTrain/2025-04-28_17-50/session_1/checkpoint.pth"
-# 断点训练-断点路径
-Breakpoint_Destination_Folder = "./experiments/PeMS03_lsfgcn/PreTrain/2025-04-16_12-59/session_1/"
+
+PRE_TRAINED_WEIGHT_PATH = "./experiments/PeMS08_PreSTGNet/PreTrain/2026-02-08_03-47/session_1/checkpoint.pth"
+
+save_dir = "./experiments/PeMS08_PreSTGNet/Train/2026-02-10_02-08/session_1/" 
 
 def trainer_distributed(args, local_rank, world_size, train_loader, valid_loader, test_loader, scaler, logger):
     # Initialise seed
     seed_it(6666)
     engine = trainer(args, scaler, local_rank, logger)
-
-    global_min_loss = 999999.
-    global_update_count = 0
     
     if args.mode == "PreTrain":
-        start_epoch = args.start_epoch_pretrain
-        epochs = args.epochs_pretrain
-        es_patience = args.es_patience_pretrain
+        checkpoint = torch.load(os.path.join(args.save_dir, "checkpoint.pth"), weights_only=True)
+
+        engine.model.load_state_dict(checkpoint['model_state_dict'])
+        engine.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        
     else:
-        start_epoch = args.start_epoch_train
-        epochs = args.epochs_train
-        es_patience = args.es_patience_train
+        checkpoint = torch.load(os.path.join(args.save_dir, "checkpoint.pth"), weights_only=True)
         
-    if 0 < start_epoch < epochs:
-        if local_rank == 0:
-            delete_target_folders(args.save_dir)
-            
-        args.save_dir = Breakpoint_Destination_Folder
-        
-        out_dir = os.path.dirname(Breakpoint_Destination_Folder)
-        log_file = "{}_breakpoint.log".format(MODE)
-        logger = setup_logging(out_dir +"/logs", log_file)
-        
-        if args.mode == "PreTrain":
-            checkpoint = torch.load(os.path.join(args.save_dir, "checkpoint.pth"), weights_only=True)
-
-            engine.model.load_state_dict(checkpoint['model_state_dict'])
-            engine.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            global_min_loss = checkpoint['loss']
-            start_epoch = checkpoint['epoch']
-            
-        else:
-            checkpoint = torch.load(os.path.join(args.save_dir, "checkpoint.pth"), weights_only=True)
-            
-            engine.predict_model.load_state_dict(checkpoint['model_state_dict'])
-            engine.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            global_min_loss = checkpoint['loss']
-            start_epoch = checkpoint['epoch']
+        engine.predict_model.load_state_dict(checkpoint['model_state_dict'])
+        engine.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     
-    # for epoch in range(start_epoch, epochs):
+    dist.barrier() 
+    if local_rank == 0:
+        logger.info("Training finished.")
         
-    #     train_start_time = time.time()
-    #     train_metrics = {
-    #         "mae": [],
-    #         "mape": [],
-    #         "rmse": [],
-    #         "wmape": [],
-    #         "r2": []
-    #     }
-        
-    #     with tqdm(enumerate(train_loader), total=len(train_loader), desc="Training Progress", unit="step", disable=(local_rank != 0), leave=False) as pbar:
-    #         for iter, dataCollections in pbar:
-    #             metrics = engine.train(dataCollections)
-                
-    #             train_metrics["mae"].append(metrics[0])
-    #             train_metrics["mape"].append(metrics[1])
-    #             train_metrics["rmse"].append(metrics[2])
-    #             train_metrics["wmape"].append(metrics[3])
-    #             train_metrics["r2"].append(metrics[4])
-                
-    #             pbar.set_postfix({
-    #                 "mae": f"{metrics[0]:.2f}",
-    #                 "rmse": f"{metrics[2]:.2f}",
-    #                 "mape": f"{metrics[1]*100:.2f}%",
-    #                 "r2": f"{metrics[4]:.2f}"
-    #             })
-    #     train_end_time = time.time()
-        
-    #     global_train_metrics = aggregate_rank_metrics(local_rank, world_size, train_metrics)
-        
-    #     # 计算训练所用时间
-    #     training_time = train_end_time - train_start_time
-        
-    #     # 打印平均值和训练时间
-    #     if local_rank == 0:
-    #         logger.info("Epoch: {:03d}, Average Metrics: Training MAE: {:.4f}, Training MAPE: {:.4f}, Training RMSE: {:.4f}, Training WMAPE: {:.4f}, Training r2: {:.4f}, Training Time: {:.4f} secs".format(
-    #                 epoch, global_train_metrics[0], global_train_metrics[1], global_train_metrics[2], global_train_metrics[3], global_train_metrics[4], training_time))
-            
-    #     valid_start_time = time.time()
-    #     valid_metrics = {
-    #         "mae": [],
-    #         "mape": [],
-    #         "rmse": [],
-    #         "wmape": [],
-    #         "r2": []
-    #     }
-        
-    #     with tqdm(enumerate(valid_loader), total=len(valid_loader), desc="Valid Progress", unit="step", disable=(local_rank != 0), leave=False) as pbar:
-    #         for iter, dataCollections in pbar:
-    #             metrics = engine.eval(dataCollections)
-                
-    #             valid_metrics["mae"].append(metrics[0])
-    #             valid_metrics["mape"].append(metrics[1])
-    #             valid_metrics["rmse"].append(metrics[2])
-    #             valid_metrics["wmape"].append(metrics[3])
-    #             valid_metrics["r2"].append(metrics[4])
-                
-    #             pbar.set_postfix({
-    #                 "mae": f"{metrics[0]:.2f}",
-    #                 "rmse": f"{metrics[2]:.2f}",
-    #                 "mape": f"{metrics[1]*100:.2f}%",
-    #                 "r2": f"{metrics[4]:.2f}"
-    #             })
-                    
-    #     valid_end_time = time.time()
-        
-    #     # 计算每个指标的平均值
-    #     global_valid_metrics = aggregate_rank_metrics(local_rank, world_size, valid_metrics)
-        
-    #     # 计算训练所用时间
-    #     validing_time = valid_end_time - valid_start_time
-        
-    #     if global_valid_metrics[0] < global_min_loss:
-            
-    #         # 同步进程
-    #         dist.barrier()
-    #         # 保存模型
-    #         save_model = engine.predict_model if args.mode == "Train" else engine.model
-            
-    #         if local_rank == 0:
-    #             new_loss = global_valid_metrics[0]
-    #             logger.info(f"Update tasks appear: {global_min_loss:.4f} > {new_loss:.4f}, -{global_min_loss-new_loss:.4f}")
-    #             logger.info("Epoch: {:03d}, Average Metrics: validing MAE: {:.4f}, validing MAPE: {:.4f}, validing RMSE: {:.4f}, validing WMAPE: {:.4f}, validing r2: {:.4f}, validing Time: {:.4f} secs".format(
-    #                 epoch, global_valid_metrics[0], global_valid_metrics[1], global_valid_metrics[2], global_valid_metrics[3], global_valid_metrics[4], validing_time))
-
-    #             global_min_loss = global_valid_metrics[0]
-    #             global_update_count = 0
-                
-    #             best_model = copy.deepcopy(save_model.state_dict())
-    #             best_optimizer = copy.deepcopy(engine.optimizer.state_dict())
-    #             torch.save({
-    #                 'epoch': epoch,
-    #                 'model_state_dict': best_model,
-    #                 'optimizer_state_dict': best_optimizer,
-    #                 'loss': global_min_loss,
-    #             }, os.path.join(args.save_dir, "checkpoint.pth"))
-            
-    #     else:
-    #         # 打印平均值和训练时间
-    #         # 同步进程
-    #         dist.barrier()
-    #         if local_rank == 0:
-    #             global_update_count += 1
-    #             logger.debug(f"Cumulative total of {global_update_count} No update tasks appear")
-    #             logger.info("Epoch: {:03d}, Average Metrics: validing MAE: {:.4f}, validing MAPE: {:.4f}, validing RMSE: {:.4f}, validing WMAPE: {:.4f}, validing r2: {:.4f}, validing Time: {:.4f} secs".format(
-    #                 epoch, global_valid_metrics[0], global_valid_metrics[1], global_valid_metrics[2], global_valid_metrics[3], global_valid_metrics[4], validing_time))
-    
-    # dist.barrier()  # 等待所有进程同步到这里
-    # if local_rank == 0:
-    #     logger.info("Training finished.")
-
-    # 加载模型，优化器，和Scaler的状态
-    save_dir = "./experiments/PeMS04_lsfgcn/Train/2025-04-29_14-33/session_1"
     checkpoint = torch.load(os.path.join(save_dir, "checkpoint.pth"), weights_only=True)
     
-    # checkpoint = torch.load(os.path.join(args.save_dir, "checkpoint.pth"), weights_only=True)
-
     save_model = engine.predict_model if args.mode == "Train" else engine.model
     
     save_model.load_state_dict(checkpoint['model_state_dict'])
@@ -219,7 +80,6 @@ def trainer_distributed(args, local_rank, world_size, train_loader, valid_loader
         test_metrics["wmape"].append(wmape)
         test_metrics["r2"].append(r2)
 
-        # 同步进程
         dist.barrier()
         mae, mape, rmse, wmape, r2 = aggregate_rank_step_metrics(local_rank, world_size, mae, mape, rmse, wmape, r2)
         
@@ -241,7 +101,6 @@ def trainer_distributed(args, local_rank, world_size, train_loader, valid_loader
             test_metrics["wmape"].append(wmape)
             test_metrics["r2"].append(r2)
 
-            # 同步进程
             dist.barrier()
             mae, mape, rmse, wmape, r2 = aggregate_rank_step_metrics(local_rank, world_size, mae, mape, rmse, wmape, r2)
             
@@ -249,13 +108,11 @@ def trainer_distributed(args, local_rank, world_size, train_loader, valid_loader
                 logger.info(
                     f"Average for Horizon {t+1}: Test MAE: {mae:.4f}, Test RMSE: {rmse:.4f}, Test MAPE: {mape:.4f}, Test WMAPE: {wmape:.4f}, Test R2: {r2:.4f}")
         
-        # 同步进程
         dist.barrier()
         global_test_metrics = aggregate_rank_metrics(local_rank, world_size, test_metrics)
         
         test_end_time = time.time()
         
-        # 计算训练所用时间
         test_time = test_end_time - test_start_time
         
         if local_rank == 0:
@@ -268,7 +125,7 @@ def main(args, logger, local_rank, world_size):
     OUTPUT_DIR = "datasets/" + args.dataset_name
 
     batch_size = 0
-    # 判别训练模式，加载对应的数据集
+ 
     if args.mode == "PreTrain":
         
         batch_size = args.batch_size_pretrain
@@ -309,7 +166,6 @@ def main(args, logger, local_rank, world_size):
             logger.info(f"valid_X shape: [{len(valid_dataset), valid_dataset[0][0].shape[0], valid_dataset[0][0].shape[1], valid_dataset[0][0].shape[2]}], valid_y shape: [{len(valid_dataset), valid_dataset[1][1].shape[0], valid_dataset[1][1].shape[1], valid_dataset[1][1].shape[2]}], valid_long shape: [{len(valid_dataset), valid_dataset[2][2].shape[0], valid_dataset[2][2].shape[1], valid_dataset[2][2].shape[2]}]")
             logger.info(f"test_X shape: [{len(test_dataset), test_dataset[0][0].shape[0], test_dataset[0][0].shape[1], test_dataset[0][0].shape[2]}], test_y shape: [{len(test_dataset), test_dataset[1][1].shape[0], test_dataset[1][1].shape[1], test_dataset[1][1].shape[2]}], test_long shape: [{len(test_dataset), test_dataset[2][2].shape[0], test_dataset[2][2].shape[1], test_dataset[2][2].shape[2]}]")
 
-    # 使用 DistributedSampler 确保每个进程都加载不同的数据子集
     train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=local_rank, shuffle=True)
     valid_sampler = DistributedSampler(valid_dataset, num_replicas=world_size, rank=local_rank, shuffle=False)
     test_sampler = DistributedSampler(test_dataset, num_replicas=world_size, rank=local_rank, shuffle=False)
@@ -319,15 +175,14 @@ def main(args, logger, local_rank, world_size):
     test_loader = DataLoader(test_dataset, batch_size=batch_size, sampler=test_sampler, num_workers=16, pin_memory=True)
     
     del train_dataset, valid_dataset, test_dataset
-    gc.collect()  # 手动触发垃圾回收
+    gc.collect()  
     
     trainer_distributed(args, local_rank, world_size, train_loader, valid_loader, test_loader, scaler, logger)
     
 if __name__ == "__main__":
     
-    # 获取环境变量 LOCAL_RANK 和 WORLD_SIZE
     local_rank = int(os.environ['LOCAL_RANK'])
-    world_size = int(os.environ['WORLD_SIZE'])  # 进程总数
+    world_size = int(os.environ['WORLD_SIZE'])  
     
     args = get_args(DATASET_NAME, MODEL_NAME, MODE)
 
@@ -359,7 +214,7 @@ if __name__ == "__main__":
         if local_rank == 0:
             logger.info("The {} training session is initiated....".format(i+1))
             
-        args.save_dir = os.path.join(out_dir, "session_" + str(i+1))
+        args.save_dir = save_dir
         os.makedirs(args.save_dir, exist_ok=True)
         
         metric_step = main(args, logger, local_rank, world_size)
